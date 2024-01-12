92959dbc3ddf0f2c1d93c2b7a84288b211575164
Ajinkya2205 <116737906+Ajinkya2205@users.noreply.github.com>
2024-01-12 22:32:22 +0530

    final commit

---
+++ app/src/main/java/com/example/dicerollarapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.dicerollarapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn : Button=findViewById(R.id.btn)
+        val img: ImageView=findViewById(R.id.dice_img)
+
+        btn.setOnClickListener(){
+            var num= Random.nextInt(1, 7)
+            if(num==1){
+                img.setImageResource(R.drawable.dice1)
+            }
+            if(num==2){
+                img.setImageResource(R.drawable.dice2)
+            }
+            if(num==3){
+                img.setImageResource(R.drawable.dice3)
+            }
+            if(num==4){
+                img.setImageResource(R.drawable.dice4)
+            }
+            if(num==5){
+                img.setImageResource(R.drawable.dice5)
+            }
+            if(num==6){
+                img.setImageResource(R.drawable.dice6)
+            }
+        }
+
+
+
+
+
+    }
+}
